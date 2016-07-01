54e6f665f7359051c82ae22203886dbe35db2376
Venkattk <Venkat.krishnaramanujam@gmail.com>
2016-07-01 11:36:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ start = int(round(time.time()))
 @app.route("/")
 def hello_world():
 
-    return "Hello world from Distelli & Docker!"
+    return "Hello from Python Docker container!"
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python simpleapp.py -p ")
